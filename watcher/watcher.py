eac2a9005d1b936884e04bf0d114e42dd483efde
Michał Wieluński <michal@w-ski.dev>
2019-08-14 14:49:48 +0200

    fix issue when credentials file does not exist

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -18,21 +18,36 @@ def main():
         tmp_value = pyperclip.paste()
         if tmp_value != recent_value:
             recent_value = tmp_value
-            match = re.fullmatch(
+            new_credentials_match = re.fullmatch(
                 re.compile(r"(?P<account>\[\d{12}_\w+\])\n%s" % schema), recent_value
             )
-            if match:
-                with open(credentials_file.as_posix(), "r") as f:
-                    file_content = f.read()
-                old_credentials = re.search(
-                    re.compile(r"(%s)\n%s" % (re.escape(match["account"]), schema)),
+            if new_credentials_match:
+                try:
+                    with open(credentials_file.as_posix(), "r") as f:
+                        file_content = f.read()
+                except FileNotFoundError:
+                    append_to_file(recent_value)
+                    continue
+                old_credentials_match = re.search(
+                    re.compile(
+                        r"(%s)\n%s"
+                        % (re.escape(new_credentials_match["account"]), schema)
+                    ),
                     file_content,
                 )
-                if old_credentials:
-                    with open(credentials_file.as_posix(), "w") as f:
-                        f.write(file_content.replace(old_credentials[0], recent_value))
+                if old_credentials_match:
+                    write_to_file(recent_value, old_credentials_match[0], file_content)
                 else:
-                    with open(credentials_file.as_posix(), "a") as f:
-                        f.write(f"\n{recent_value}")
+                    append_to_file(recent_value)
 
         time.sleep(0.5)
+
+
+def write_to_file(new_credentials: str, old_credentials: str, file_content: str):
+    with open(credentials_file.as_posix(), "w") as f:
+        f.write(file_content.replace(old_credentials[0], new_credentials))
+
+
+def append_to_file(credentials: str):
+    with open(credentials_file.as_posix(), "a") as f:
+        f.write(f"\n{credentials}")
