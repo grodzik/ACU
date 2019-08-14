833b8f8aa7b61c63f22104d4e692b80b8570b990
Michał Wieluński <michal@w-ski.dev>
2019-08-14 14:29:46 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="aws_credentials_watcher",
     version="0.0.1",
     license="MIT",
+    url="https://github.com/landmaj/aws_credentials_watcher",
+    description="Automatically update AWS credentials file.",
     author="Michał Wieluński",
     author_email="michal@w-ski.dev",
     packages=find_packages(),
