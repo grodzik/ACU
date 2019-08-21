b3d878e4fa61b6becb4ae9ca5315e3bcedb1b4a8
Michał Wieluński <michal@w-ski.dev>
2019-08-21 15:58:21 +0200

    fix tests exclusion from build

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Automatically update AWS credentials file.",
     author="Michał Wieluński",
     author_email="michal@w-ski.dev",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(exclude=["test*"]),
     install_requires=["pyperclip==1.7.0"],
     extras_require={"dev": ["pytest==5.1.1", "importlib-resources==1.0.2"]},
     entry_points={"console_scripts": ["acu=watcher.watcher:main"]},
