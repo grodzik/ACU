8802664c630ea8e0b0d0a176c1555b87201e0813
Michał Wieluński <michal@w-ski.dev>
2019-08-21 15:44:11 +0200

    add README.md and update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="aws_credentials_watcher",
-    version="0.0.1",
+    name="ACU",
+    version="0.1.0",
     license="MIT",
     url="https://github.com/landmaj/aws_credentials_watcher",
     description="Automatically update AWS credentials file.",
     author="Michał Wieluński",
     author_email="michal@w-ski.dev",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=["pyperclip==1.7.0"],
     extras_require={"dev": ["pytest==5.1.1", "importlib-resources==1.0.2"]},
-    entry_points={"console_scripts": ["aws_credentials_watcher=watcher.watcher:main"]},
+    entry_points={"console_scripts": ["acu=watcher.watcher:main"]},
 )
