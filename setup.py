84ee66dc97e8aad5c931615d2c73ff7677eb2624
Michał Wieluński <michal@w-ski.dev>
2019-08-21 15:56:23 +0200

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -12,4 +12,10 @@ setup(
     install_requires=["pyperclip==1.7.0"],
     extras_require={"dev": ["pytest==5.1.1", "importlib-resources==1.0.2"]},
     entry_points={"console_scripts": ["acu=watcher.watcher:main"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
 )
